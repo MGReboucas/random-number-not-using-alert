6d320b60ad982bea95efd5a8dde4697a2420dd3b
Matheus <reboucas444@gmail.com>
2025-06-23 08:27:36 -0300

    mechanics check if kick is equal to secret number

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let numeroSecreto = gerarNumeroAleatorio();
 //let paragrafo = document.querySelector('p');
 //paragrafo.innerHTML = 'Escolha o numero entre 1 e 10';
 
-function exibirTextoNaTela(tag, texto){
+function exibirTextoNaTela(tag, texto) {
      let campo = document.querySelector(tag);
      campo.innerHTML = texto;
 }
@@ -16,9 +16,19 @@ exibirTextoNaTela('p', 'Escolha um número de 1 a 10');
 
 function verificarChute() {
      let chute = document.querySelector('input').value;
-     console.log(chute == numeroSecreto)
+
+     if (chute == numeroSecreto) {
+          exibirTextoNaTela('h1', 'Acertou!');
+          exibirTextoNaTela('p', 'Voce descobriu o numero secreto!')
+     } else {
+          if (chute > numeroSecreto) {
+               exibirTextoNaTela('p', 'Errou, numero secreto é menor');
+          } else {
+               exibirTextoNaTela('p', 'Errou, numero secreto é meior');
+          }
+     }
 }
 
-function gerarNumeroAleatorio(){
+function gerarNumeroAleatorio() {
      return parseInt(Math.random() * 10 + 1);
 }
